5f302ed8cf96f86348a1ce70a9b286e07378c708
mrshau <nagoevislam2711@gmail.com>
2024-11-13 19:58:49 +0300

    Добавьте файлы проекта.

---
+++ ProvodnikApp/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿using System.Windows.Forms;
+
+namespace ProvodnikApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ListView listViewFiles;
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listViewFiles = new System.Windows.Forms.ListView();
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.renameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.propertiesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxDerictory = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listViewFiles
+            // 
+            this.listViewFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listViewFiles.ContextMenuStrip = this.contextMenuStrip;
+            this.listViewFiles.HideSelection = false;
+            this.listViewFiles.Location = new System.Drawing.Point(321, 45);
+            this.listViewFiles.Name = "listViewFiles";
+            this.listViewFiles.Size = new System.Drawing.Size(601, 393);
+            this.listViewFiles.TabIndex = 0;
+            this.listViewFiles.UseCompatibleStateImageBehavior = false;
+            this.listViewFiles.View = System.Windows.Forms.View.Details;
+            this.listViewFiles.DoubleClick += new System.EventHandler(this.listViewFiles_DoubleClick);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.renameToolStripMenuItem,
+            this.copyToolStripMenuItem,
+            this.pasteToolStripMenuItem,
+            this.refreshToolStripMenuItem,
+            this.deleteToolStripMenuItem,
+            this.propertiesToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(162, 158);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Image = global::ProvodnikApp.Properties.Resources.OpenIcon;
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.openToolStripMenuItem.Text = "Открыть";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenItem_Click);
+            // 
+            // renameToolStripMenuItem
+            // 
+            this.renameToolStripMenuItem.Image = global::ProvodnikApp.Properties.Resources.RenameIcon;
+            this.renameToolStripMenuItem.Name = "renameToolStripMenuItem";
+            this.renameToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.renameToolStripMenuItem.Text = "Переименовать";
+            this.renameToolStripMenuItem.Click += new System.EventHandler(this.RenameItem_Click);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Image = global::ProvodnikApp.Properties.Resources.CopyIcon;
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.copyToolStripMenuItem.Text = "Копировать";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.CopyItem_Click);
+            // 
+            // pasteToolStripMenuItem
+            // 
+            this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            this.pasteToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.pasteToolStripMenuItem.Text = "Вставить";
+            this.pasteToolStripMenuItem.Click += new System.EventHandler(this.PasteItem_Click);
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Image = global::ProvodnikApp.Properties.Resources.RefreshIcon;
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.refreshToolStripMenuItem.Text = "Обновить";
+            this.refreshToolStripMenuItem.Click += new System.EventHandler(this.RefreshItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Image = global::ProvodnikApp.Properties.Resources.DeleteIcon;
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.deleteToolStripMenuItem.Text = "Удалить";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.DeleteItem_Click);
+            // 
+            // propertiesToolStripMenuItem
+            // 
+            this.propertiesToolStripMenuItem.Image = global::ProvodnikApp.Properties.Resources.SettingIcon;
+            this.propertiesToolStripMenuItem.Name = "propertiesToolStripMenuItem";
+            this.propertiesToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.propertiesToolStripMenuItem.Text = "Свойства";
+            this.propertiesToolStripMenuItem.Click += new System.EventHandler(this.PropertiesItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.Image = global::ProvodnikApp.Properties.Resources.BackIcon;
+            this.button1.Location = new System.Drawing.Point(321, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(32, 32);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBoxDerictory
+            // 
+            this.textBoxDerictory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxDerictory.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxDerictory.Location = new System.Drawing.Point(359, 16);
+            this.textBoxDerictory.Name = "textBoxDerictory";
+            this.textBoxDerictory.Size = new System.Drawing.Size(563, 23);
+            this.textBoxDerictory.TabIndex = 2;
+            this.textBoxDerictory.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxDerictory_KeyDown);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(12, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(283, 24);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Проводник";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button6
+            // 
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.Location = new System.Drawing.Point(12, 291);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(283, 55);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Каталог \"Program Files (x86)\"";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.Location = new System.Drawing.Point(12, 230);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(283, 55);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Каталог \"System\"";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Location = new System.Drawing.Point(12, 169);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(283, 55);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Каталог \"Windows\"";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Location = new System.Drawing.Point(12, 108);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(283, 55);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Каталог \"Program Files\"";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button7
+            // 
+            this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button7.Location = new System.Drawing.Point(12, 47);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(283, 55);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Каталог \"Пользователи\"";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(934, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBoxDerictory);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listViewFiles);
+            this.MinimumSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
+            this.Text = "Просмотр папки";
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxDerictory;
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem renameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem propertiesToolStripMenuItem;
+        private Button button2;
+        private Button button6;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button7;
+    }
+}
+
---
+++ ProvodnikApp/Form1.cs
@@ -0,0 +1,395 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProvodnikApp
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("shell32.dll", CharSet = CharSet.Auto)]
+        private static extern bool ShellExecuteEx(ref SHELLEXECUTEINFO lpExecInfo);
+
+        private const int SW_SHOW = 5;
+        private const uint SEE_MASK_INVOKEIDLIST = 0x0000000C;
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        private struct SHELLEXECUTEINFO
+        {
+            public int cbSize;
+            public uint fMask;
+            public IntPtr hwnd;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpVerb;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpFile;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpParameters;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpDirectory;
+            public int nShow;
+            public IntPtr hInstApp;
+            public IntPtr lpIDList;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpClass;
+            public IntPtr hkeyClass;
+            public uint dwHotKey;
+            public IntPtr hIcon;
+            public IntPtr hProcess;
+        }
+
+        private string _folderPath = null;
+        private string _copiedPath = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.listViewFiles.Columns.Add("Имя", 250, HorizontalAlignment.Left);
+            this.listViewFiles.Columns.Add("Тип", 100, HorizontalAlignment.Left);
+            this.listViewFiles.Columns.Add("Размер", 100, HorizontalAlignment.Right);
+            this.listViewFiles.Columns.Add("Дата изменения", 150, HorizontalAlignment.Left);
+
+            ImageList imageList = new ImageList();
+            imageList.Images.Add("folder", Properties.Resources.FolderIcon);
+            imageList.Images.Add("file", Properties.Resources.FileIcon);
+            listViewFiles.LargeImageList = imageList;
+            listViewFiles.SmallImageList = imageList;
+
+            LoadFolderContent();
+
+        }
+
+        private void LoadFolderContent()
+        {
+            this.Text = $"Содержимое папки: {_folderPath}";
+
+            listViewFiles.Items.Clear();
+
+            try
+            {
+                if (_folderPath == null)
+                    return;
+                textBoxDerictory.Text = _folderPath;
+
+                var directories = Directory.GetDirectories(_folderPath);
+                var files = Directory.GetFiles(_folderPath);
+
+                foreach (var dir in directories)
+                {
+                    var dirInfo = new DirectoryInfo(dir);
+                    var item = new ListViewItem(dirInfo.Name)
+                    {
+                        ImageIndex = 0, 
+                        Tag = dir,       
+                    };
+
+                    item.SubItems.Add("Папка"); // Тип
+                    item.SubItems.Add("");      // Размер для папки оставить пустым
+                    item.SubItems.Add(dirInfo.LastWriteTime.ToString()); // Дата изменения
+
+                    listViewFiles.Items.Add(item);
+                }
+
+                // Добавляем файлы в ListView
+                foreach (var file in files)
+                {
+                    var fileInfo = new FileInfo(file);
+                    var item = new ListViewItem(fileInfo.Name)
+                    {
+                        ImageIndex = 1,
+                        Tag = file       
+                    };
+                    item.SubItems.Add(fileInfo.Extension + " файл"); // Тип файла
+                    item.SubItems.Add(fileInfo.Length.ToString() + " байт"); // Размер
+                    item.SubItems.Add(fileInfo.LastWriteTime.ToString()); // Дата изменения
+                    listViewFiles.Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при загрузке содержимого папки: {ex.Message}");
+                var parentDirectory = Directory.GetParent(_folderPath);
+                if (parentDirectory != null)
+                {
+                    _folderPath = parentDirectory.FullName;
+                    LoadFolderContent();
+                }
+            }
+        }
+
+        // Двойное нажатие на папку\каталог
+        private void listViewFiles_DoubleClick(object sender, EventArgs e)
+        {
+            if (listViewFiles.SelectedItems.Count > 0)
+            {
+                var selectedItem = listViewFiles.SelectedItems[0];
+                var path = selectedItem.Tag.ToString();
+
+                if (Directory.Exists(path))
+                {
+                    _folderPath = path;
+                    LoadFolderContent();
+                }
+                else if (File.Exists(path))
+                {
+                    try
+                    {
+                        System.Diagnostics.Process.Start(path);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Не удалось открыть файл !\n{ex.Message}");
+                    }
+                }
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            var parentDirectory = Directory.GetParent(_folderPath);
+            if (parentDirectory != null)
+            {
+                _folderPath = parentDirectory.FullName;
+                LoadFolderContent();
+            }
+            else
+            {
+                MessageBox.Show("Вы уже находитесь в корневой директории.");
+            }
+        }
+
+        // Открыть
+        private void OpenItem_Click(object sender, EventArgs e)
+        {
+            if (listViewFiles.SelectedItems.Count > 0)
+            {
+                var selectedItem = listViewFiles.SelectedItems[0];
+                var path = selectedItem.Tag as string;
+
+                if (File.Exists(path) || Directory.Exists(path))
+                {
+                    Process.Start("explorer", path);
+                }
+                else
+                {
+                    MessageBox.Show("Файл или папка не найдены.");
+                }
+            }
+        }
+
+        // Обновить
+        private void RefreshItem_Click(object sender, EventArgs e)
+        {
+            LoadFolderContent();
+        }
+
+        // Удалить
+        private void DeleteItem_Click(object sender, EventArgs e)
+        {
+            if (listViewFiles.SelectedItems.Count > 0)
+            {
+                var selectedItem = listViewFiles.SelectedItems[0];
+                var path = selectedItem.Tag as string;
+
+                var result = MessageBox.Show("Вы уверены, что хотите удалить этот элемент?", "Удаление", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    try
+                    {
+                        if (File.Exists(path))
+                        {
+                            File.Delete(path);
+                        }
+                        else if (Directory.Exists(path))
+                        {
+                            Directory.Delete(path, true);
+                        }
+                        LoadFolderContent();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Ошибка при удалении: {ex.Message}");
+                    }
+                }
+            }
+        }
+
+        // Переименовать
+        private void RenameItem_Click(object sender, EventArgs e)
+        {
+            if (listViewFiles.SelectedItems.Count > 0)
+            {
+                var selectedItem = listViewFiles.SelectedItems[0];
+                var oldPath = selectedItem.Tag as string;
+
+                if (oldPath != null)
+                {
+                    string newName = Microsoft.VisualBasic.Interaction.InputBox("Введите новое имя:", "Переименовать", selectedItem.Text);
+                    if (!string.IsNullOrEmpty(newName))
+                    {
+                        string newPath = Path.Combine(Path.GetDirectoryName(oldPath), newName);
+
+                        try
+                        {
+                            if (File.Exists(oldPath))
+                            {
+                                File.Move(oldPath, newPath);
+                            }
+                            else if (Directory.Exists(oldPath))
+                            {
+                                Directory.Move(oldPath, newPath);
+                            }
+                            LoadFolderContent();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"Ошибка при переименовании: {ex.Message}");
+                        }
+                    }
+                }
+            }
+        }
+
+        // Копировать
+        private void CopyItem_Click(object sender, EventArgs e)
+        {
+            if (listViewFiles.SelectedItems.Count > 0)
+            {
+                var selectedItem = listViewFiles.SelectedItems[0];
+                _copiedPath = selectedItem.Tag as string;
+                MessageBox.Show("Элемент скопирован.");
+            }
+        }
+
+        // Вставить
+        private void PasteItem_Click(object sender, EventArgs e)
+        {
+            if (_copiedPath == null)
+            {
+                MessageBox.Show("Буфер обмена пуст.");
+                return;
+            }
+
+            var destinationPath = Path.Combine(_folderPath, Path.GetFileName(_copiedPath));
+
+            try
+            {
+                if (Directory.Exists(_copiedPath))
+                {
+                    CopyDirectory(_copiedPath, destinationPath);
+                }
+                else if (File.Exists(_copiedPath))
+                {
+                    File.Copy(_copiedPath, destinationPath, overwrite: true);
+                }
+                LoadFolderContent();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при вставке: {ex.Message}");
+            }
+        }
+
+        private void CopyDirectory(string sourceDir, string destinationDir)
+        {
+            Directory.CreateDirectory(destinationDir);
+
+            foreach (string file in Directory.GetFiles(sourceDir))
+            {
+                string destFile = Path.Combine(destinationDir, Path.GetFileName(file));
+                File.Copy(file, destFile, overwrite: true);
+            }
+
+            foreach (string directory in Directory.GetDirectories(sourceDir))
+            {
+                string destDir = Path.Combine(destinationDir, Path.GetFileName(directory));
+                CopyDirectory(directory, destDir);
+            }
+        }
+
+        // Свойства
+        private void PropertiesItem_Click(object sender, EventArgs e)
+        {
+            if (listViewFiles.SelectedItems.Count > 0)
+            {
+                var selectedItem = listViewFiles.SelectedItems[0];
+                var path = selectedItem.Tag as string;
+
+                if (File.Exists(path) || Directory.Exists(path))
+                {
+                    ShowFileProperties(path);
+                }
+            }
+        }
+
+        private void ShowFileProperties(string path)
+        {
+            SHELLEXECUTEINFO info = new SHELLEXECUTEINFO();
+            info.cbSize = Marshal.SizeOf(info);
+            info.lpVerb = "properties";
+            info.lpFile = path;
+            info.nShow = SW_SHOW;
+            info.fMask = SEE_MASK_INVOKEIDLIST;
+
+            if (!ShellExecuteEx(ref info))
+            {
+                MessageBox.Show("Не удалось открыть окно свойств.");
+            }
+        }
+
+        
+
+        private void textBoxDerictory_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                _folderPath = textBoxDerictory.Text;
+                LoadFolderContent();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            _folderPath = @"C:\Users";
+            LoadFolderContent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            _folderPath = @"C:\Program Files";
+            LoadFolderContent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            _folderPath = @"C:\Windows";
+            LoadFolderContent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            _folderPath = @"C:\Windows\System";
+            LoadFolderContent();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            _folderPath = @"C:\Program Files (x86)";
+            LoadFolderContent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer.exe", $"/select, \"{_folderPath}\"");
+        }
+    }
+}
